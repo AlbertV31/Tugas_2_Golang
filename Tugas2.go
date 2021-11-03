604be2cf2356abcc37f86c190b5ff7c5b5d9f00f
Albert <albert.bagil@gmail.com>
2021-11-03 19:16:33 +0700

    Tugas 2 Golang
    Tugas 2 Golang untuk dikumpulkan ke Niomic

---
+++ Tugas2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+  var angka = 7
+  if angka % 2 == 0 {
+      fmt.Printf("Bilangan Genap")
+    }else{
+      fmt.Printf("Bilangan Ganjil")
+    }
+}
